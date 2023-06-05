0ae13453bf1c04c9f872286c96cabd2cfecf1fd5
Justin <jwei@lcs.on.ca>
2023-06-05 09:28:15 -0400

    Initial Commit

---
+++ PlayerTracker/PlayerTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerTrackerApp.swift
+//  PlayerTracker
+//
+//  Created by Justin Zack Wei on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct PlayerTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
