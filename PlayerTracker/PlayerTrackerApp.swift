8d2382f66ab34be46f5f1c18e2808e7d7915c393
Justin <jwei@lcs.on.ca>
2023-06-05 09:31:01 -0400

    Deleted ContentView and replaced with PlayerTrackerView

--- PlayerTracker/PlayerTrackerApp.swift
+++ PlayerTracker/PlayerTrackerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PlayerTrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlayerTrackerView()
         }
     }
 }
---
+++ PlayerTracker/PlayerTrackerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerTrackerView.swift
+//  PlayerTracker
+//
+//  Created by Justin Zack Wei on 2023-06-05.
+//
+
+import SwiftUI
+
+struct PlayerTrackerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerTrackerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerTrackerView()
+    }
+}
