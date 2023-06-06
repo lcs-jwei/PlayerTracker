7066831ebb9b3c447eafeae4e617ef1e46387cb2
Justin <jwei@lcs.on.ca>
2023-06-06 19:51:51 -0400

    Made an add player button that makes the screen for adding a player pop up. add player still not working though.

--- PlayerTracker/PlayerTrackerView.swift
+++ PlayerTracker/PlayerTrackerView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 import Blackbird
 
 struct PlayerTrackerView: View {
-//MARK: STORED PROPERTIES
+    //MARK: STORED PROPERTIES
     
     @State var score = 0
     @State var oscore = 0
     @State var plmi = 0
-    @State var tName = ""
+    @State var tName = "Team Name"
+    @State var showingAddPlayerView = false
     
     let columns = [
         GridItem(.flexible()),
@@ -28,128 +29,150 @@ struct PlayerTrackerView: View {
     //MARK: COMPUTED PROPERTIES
     
     var body: some View {
-        
-        ZStack{
+        NavigationView{
             
-            
-            VStack{
-                HStack{
-                    Button(action: {score += 1
-                        plmi += 1
-                        
-                    }) {
-                        Text("GOAL")
-                            .font(Font.custom("MarkerFelt-Thin", size: 50))
-                        
-                            .foregroundColor(.white)
+            ZStack{
+                
+                
+                VStack{
+                    HStack{
+                        Button(action: {score += 1
+                            plmi += 1
+                            
+                        }) {
+                            Text("GOAL")
+                                .font(Font.custom("MarkerFelt-Thin", size: 50))
+                            
+                                .foregroundColor(.white)
+                                .padding()
+                            
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .padding()
+                        }
+                        Text("\(score)-\(oscore)")
+                            .font(Font.custom("MarkerFelt-Thin", size: 100))
                             .padding()
+                        Button(action: {oscore += 1
+                            plmi += -1
+                            
+                        }) {
+                            Text("GOAL")
+                                .font(Font.custom("MarkerFelt-Thin", size: 50))
+                            
+                                .foregroundColor(.white)
+                                .padding()
+                            
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .padding()
+                        }
                         
-                            .background(Color.green)
-                            .cornerRadius(10)
-                            .padding()
-                    }
-                    Text("\(score)-\(oscore)")
-                        .font(Font.custom("MarkerFelt-Thin", size: 100))
-                        .padding()
-                    Button(action: {oscore += 1
-                        plmi += -1
-                        
-                    }) {
-                        Text("GOAL")
-                            .font(Font.custom("MarkerFelt-Thin", size: 50))
-                        
-                            .foregroundColor(.white)
-                            .padding()
-                        
-                            .background(Color.green)
-                            .cornerRadius(10)
-                            .padding()
                     }
                     
-                }
-                
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(0..<100) { i in
-                            Button(action: {
-                                
-                            }){
-                                List(Players.results){currentPlayer in
-                                    PlayerView(name: currentPlayer.name,
-                                               number: currentPlayer.number,
-                                               plusminus: currentPlayer.plusminus,
-                                               time: currentPlayer.time
-                                               
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-                            .padding(40)
-                            
-                            Button(action: {score = 0
-                                oscore = 0
-                                plmi = 0
-                                
-                            }) {
-                                Text("Game End")
-                                    .font(Font.custom("MarkerFelt-Thin", size: 30))
-                                
-                                    .foregroundColor(.white)
-                                    .padding()
-                                
-                                    .background(Color.red)
-                                    .cornerRadius(10)
-                                    .padding()
-                            }
-                        }
-                        VStack{
-                            HStack{
-                                VStack{
-                                    Text("Ajax Knights")
-                                        .font(Font.custom("MarkerFelt-Thin", size: 50))
-                                        .padding()
-                                    
-                                        .border(Color.red, width: 10)
-                                        .padding(.leading)
-                                    
-                                    
-                                }
-                                
-                                Spacer()
-                                Text("Opponent")
-                                    .font(Font.custom("MarkerFelt-Thin", size: 50))
-                                    .padding()
-                                
-                                    .border(Color.red, width: 10)
-                                    .padding()
-                            }
-                            HStack{
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 16) {
+                            ForEach(0..<100) { i in
                                 Button(action: {
                                     
-                                }) {
-                                    Text("Change Team Name")
-                                        .font(Font.custom("MarkerFelt-Thin", size: 20))
-                                    
-                                        .foregroundColor(.white)
-                                        .padding(10)
-                                    
-                                        .background(Color.blue)
-                                        .cornerRadius(10)
-                                        .padding(.leading)
+                                }){
+                                    List(Players.results){currentPlayer in
+                                        PlayerView(name: currentPlayer.name,
+                                                   number: currentPlayer.number,
+                                                   plusminus: currentPlayer.plusminus,
+                                                   time: currentPlayer.time
+                                                   
+                                        )
+                                    }
                                 }
-                                Spacer()
-                                
                             }
-                            Spacer()
-                            
+                        }
+                        
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .bottomBar) {
+                            Button(action: {
+                                showingAddPlayerView = true
+                            }, label: {
+                                HStack{
+                                    Image(systemName: "person.circle")
+                                    Text("Add Player")
+                                        
+                                }.padding()
+                                    .background(Color.blue)
+                                    .cornerRadius(10)
+                                    .foregroundColor(.white)
+                            })
+                            .sheet(isPresented: $showingAddPlayerView) {
+                                AddPlayerView()
+                                    .presentationDetents([.fraction(0.3)])
+                            }
                         }
                     }
+                    .padding(40)
+                    
+                    Button(action: {score = 0
+                        oscore = 0
+                        plmi = 0
+                        
+                    }) {
+                        Text("Game End")
+                            .font(Font.custom("MarkerFelt-Thin", size: 30))
+                        
+                            .foregroundColor(.white)
+                            .padding()
+                        
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+                }
+                VStack{
+                    HStack{
+                        VStack{
+                            Text("Ajax Knights")
+                                .font(Font.custom("MarkerFelt-Thin", size: 50))
+                                .padding()
+                            
+                                .border(Color.red, width: 10)
+                                .padding(.leading)
+                            
+                            
+                        }
+                        
+                        Spacer()
+                        Text("Opponent")
+                            .font(Font.custom("MarkerFelt-Thin", size: 50))
+                            .padding()
+                        
+                            .border(Color.red, width: 10)
+                            .padding()
+                    }
+                    HStack{
+                        Button(action: {
+                            
+                        }) {
+                            Text("Change Team Name")
+                                .font(Font.custom("MarkerFelt-Thin", size: 20))
+                            
+                                .foregroundColor(.white)
+                                .padding(10)
+                            
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                                .padding(.leading)
+                        }
+                        Spacer()
+                        
+                    }
+                    Spacer()
+                    
                 }
             }
-        
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
     
+}
 
 struct PlayerTrackerView_Previews: PreviewProvider {
     static var previews: some View {
