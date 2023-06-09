612130b354254c7cd4fb6a426a8fde1d2d1309df
Justin <jwei@lcs.on.ca>
2023-06-09 09:07:33 -0400

    Changed to blue somewhere in UI

--- PlayerTracker/PlayerTrackerView.swift
+++ PlayerTracker/PlayerTrackerView.swift
@@ -97,7 +97,7 @@ struct PlayerTrackerView: View {
                             Text("GOAL")
                                 .font(Font.custom("MarkerFelt-Thin", size: 50))
                             
-                                .foregroundColor(.white)
+                                .foregroundColor(.blue)
                                 .padding()
                             
                                 .background(Color.green)
